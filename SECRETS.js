88992b6d02a5dcc3adfeec30545802c4f21a21c7
pierreparrot0-blip <pierreparrot0@gmail.com>
2026-01-13 15:59:09 +0100

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -7,17 +7,4 @@
 // const ACCESS_SECRET = "YOUR_TWITTER_ACCESS_SECRET";
 // const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY";
 
-const APP_KEY = "jrzAIKpvhq33pN0QgXlS7hBrk";
-const APP_SECRET = "xnBqThQ1C6d0yiihDBy0ykPpRTZkyj7JlR3PaJPpTGPKNY4rNQ";
-const ACCESS_TOKEN = "2011023472395530240-a5FK19309MbU31rETTM92rOAEIKd25";
-const ACCESS_SECRET = "PPt1y9yYsGZOPDn8kWcF5qKH6Ii9lfrEzKybXBUQ67Sra";
-
-const SECRETS = {
-  APP_KEY,
-  APP_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_SECRET,
-  //GEMINI_API_KEY,
-};
-
-module.exports = SECRETS;
+// Set the values in Settings → Secrets and variables → Actions → New repository secret
